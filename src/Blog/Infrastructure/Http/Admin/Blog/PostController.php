3dcdf4270246c8204cfad6d61189ece73a73d81f
Vladyslav Honchar <dr.tienam123@gmail.com>
2025-10-25 01:41:38 +0000

    chore(blog): clean up debug code in PostController

--- src/Blog/Infrastructure/Http/Admin/Blog/PostController.php
+++ src/Blog/Infrastructure/Http/Admin/Blog/PostController.php
@@ -4,17 +4,24 @@ declare(strict_types=1);
 
 namespace App\Blog\Infrastructure\Http\Admin\Blog;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-#[Route('/admin/blog', name: 'admin.blog.posts.')]
+#[Route('/blog', name: 'admin.blog.posts.')]
 class PostController
 {
     #[Route('/posts', name: 'listAll', methods: ['GET'])]
     public function index(): Response
     {
+        $date = "10-10-2025";
+       dump($date);
+       $toArray = explode("-", $date);
+       dump($toArray);
+       $implodeToDots = implode(".", $toArray);
+       dd($implodeToDots);
+
+           dd(11);
         return new JsonResponse([
             'code' => Response::HTTP_OK,
         ]);

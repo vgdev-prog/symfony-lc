56cd3e5186bc71f178e8eae44a78af3473392a0e
Vladyslav Honchar <dr.tienam123@gmail.com>
2025-10-25 01:40:55 +0000

    feat(common): add Locale enum with supported languages

---
+++ src/Common/Domain/Locale.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Common\Domain;
+
+/**
+ * Supported application locales.
+ *
+ * This enum defines all available language options for the application.
+ * Used for content translation, UI localization, and user preferences.
+ */
+enum Locale: string
+{
+    case ENGLISH = 'en';
+
+    case RUSSIAN = 'ru';
+
+    case UKRAINIAN = 'ua';
+
+    case POLAND = 'pl';
+
+    /**
+     * Returns all available locale codes.
+     *
+     * Useful for validation, dropdowns, and checking if a locale is supported.
+     *
+     * @return array<int, string> Array of locale codes ['en', 'ru', 'ua', 'pl']
+     */
+    public static function languages(): array
+    {
+        return array_column(self::cases(), 'value');
+    }
+}

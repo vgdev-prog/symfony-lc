471788d6d519543464a6dd4cd68ad85401bec0a8
Vladyslav Honchar <dr.tienam123@gmail.com>
2025-10-25 01:41:31 +0000

    feat(blog): add translation support to Post entity

--- src/Blog/Domain/Entity/Post.php
+++ src/Blog/Domain/Entity/Post.php
@@ -6,17 +6,24 @@ namespace App\Blog\Domain\Entity;
 
 use App\Blog\Domain\ValueObject\Post\PostId;
 use App\Blog\Domain\ValueObject\Post\PostStatus;
+use App\Common\Domain\Entity\Model;
+use App\Common\Domain\Locale;
+use App\Common\Domain\Trait\Translatable;
 use DateTimeImmutable;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
+use Gedmo\Mapping\Annotation as Gedmo;
 
 #[ORM\Entity()]
-class Post
+class Post extends Model
 {
+    use Translatable;
+
     #[ORM\Id]
     #[ORM\Column(type: 'uuid', unique: true)]
     private string $id;
 
+    #[Gedmo\Translatable]
     #[ORM\Column(length: 255)]
     private string $title;
 
@@ -49,6 +56,17 @@ class Post
     public function id(): PostId
     {
         return PostId::fromString($this->id);
+    }
+
+    public function changeTitle(string $title, Locale $locale): void
+    {
+        $this->setTranslatableLocale($locale);
+        $this->title = $title;
+    }
+
+    public function changeDescription(string $description): void
+    {
+        $this->description = $description;
     }
 
     public function publish(): void
